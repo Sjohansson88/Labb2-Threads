7dce4336e657a13476f29d146e604aaf3c57d8a5
Sjohansson88 <sjohansson88@hotmail.com>
2024-04-21 18:31:56 +0200

    adjusting

--- Program.cs
+++ Program.cs
@@ -11,13 +11,13 @@
             var race1Task = car1.StartRaceAsync();
             var race2Task = car2.StartRaceAsync();
 
-            // Starta en oändlig loop för att lyssna på användarinput
+          
             while (true)
             {
                 Console.WriteLine("Type 'status' to get race status:");
                 string input = Console.ReadLine();
 
-                // Om användaren skrev in "status", skriv ut status för båda bilarna
+                
                 if (input.ToLower() == "status")
                 {
                     Console.WriteLine("");
@@ -26,26 +26,26 @@
                     car2.PrintStatus();
                     Console.WriteLine("");
                 }
-                // Vänta på att antingen race1Task eller race2Task ska slutföras eller användaren ska skriva in "status"
+               
                 await Task.WhenAny(race1Task, race2Task);
 
-                // Kontrollera om någon av racen är klara
+              
                 if (car1.Finished || car2.Finished)
                 {
-                    break; // Om någon bil har gått i mål, avsluta loopen
+                    break; 
                 }
 
-                // Läs in användarinput
+                
             
             }
 
-            // När loopen avslutas har en vinnare blivit bestämd, skriv ut vinnaren
+        
             string winner = car1.Finished ? car1.Name :
                             car2.Finished ? car2.Name :
                             "No winner";
             Console.WriteLine($"The winner is {winner}!");
 
-            // Skriv ut slutlig status för båda bilarna
+           
             Console.WriteLine("");
             car1.PrintStatus();
             Console.WriteLine("");
